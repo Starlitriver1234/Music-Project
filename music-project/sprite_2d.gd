3addb819fff0e5dd38f2af90268124968cdc2aad
Starlitriver1234 <C24745999@mytudublin.ie>
2024-12-07 17:54:45 +0000

    Audio works! (only one key)

--- music-project/sprite_2d.gd
+++ music-project/sprite_2d.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 var key : Texture
 var key2 : Texture
-
+@onready var audio_player = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	key = texture
@@ -12,8 +12,10 @@ func _ready() -> void:
 
 
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		texture = key2
-	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
-		texture = key
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			texture = key2
+			audio_player.play()
+		else:
+			texture = key
 	pass
