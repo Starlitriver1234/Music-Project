e99cfdac2d97203bb5758e82f00499f38d744ca1
Starlitriver1234 <C24745999@mytudublin.ie>
2024-12-07 17:07:57 +0000

    Added a key and a bit of code

---
+++ music-project/sprite_2d.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+
+var key : Texture
+var key2 : Texture
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	key = texture
+	key2 = load("res://Assets/key2.png")
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		texture = key2
+	pass
